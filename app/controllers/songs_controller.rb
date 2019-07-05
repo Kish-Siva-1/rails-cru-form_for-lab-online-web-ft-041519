c6742fdc3c2fea930fec7d2b01f310abf1dd440b
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-05 19:09:23 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,24 @@
+class SongsController < ApplicationController
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(params.require(:song).permit(:name))
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+end
